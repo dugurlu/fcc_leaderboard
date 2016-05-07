375f197da14f990a5c8f1c7ddd5f8d6ef4556b1d
Deniz Ugurlu <mail@denizugurlu.de>
2016-05-07 22:25:45 +0200

    import scaffolding for new project

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const css = require('../css/style.scss');
+const app = document.getElementById('app');
+
+ReactDOM.render(
+    <h1>Hello World!</h1>
+  ,app);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+const config = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/index.js",
+  module: {
+    loaders: [
+      // JAVASCRIPT / BABEL
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        }
+      },
+      // CSS / SASS
+      {
+        test: /\.s?css$/,
+        loaders: ['style', 'css', 'sass', 'postcss']
+      },
+    ]
+  },
+  output: {
+    path: path.join(__dirname, './src'),
+    filename: 'index.min.js',
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+  postcss: function() {
+    return [autoprefixer]
+  }
+}
+
+module.exports = config
